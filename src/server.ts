ed6f4fcd3b998542d8b56303e3356b1afdaddd8f
elton de campos <eltoncampos36@gmail.com>
2021-02-25 20:56:19 -0300

    feat: create get route for test

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+    return response.send('Server OK')
+})
+
 app.listen(3333, () => {
     console.log('Server is running');
-})
+});
