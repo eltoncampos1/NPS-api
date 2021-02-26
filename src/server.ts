81c834960180e83119000c945c9566f0f0fe114d
elton de campos <eltoncampos36@gmail.com>
2021-02-25 21:07:49 -0300

    feat: create post route for test

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,11 @@ app.get('/', (request, response) => {
     return response.send('Server OK')
 })
 
+
+app.post('/', (request, response) => {
+    return response.json({ message: 'Dados alterados'} )
+})
+
 app.listen(3333, () => {
     console.log('Server is running');
 });
