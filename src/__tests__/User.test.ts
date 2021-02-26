908dc87dc607c7ad6e95dfe50ce3bfd89831fa05
elton de campos <eltoncampos36@gmail.com>
2021-02-26 00:57:36 -0300

    feat: create user test

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import { app } from '../app';
+
+import createConnection from '../database';
+
+describe('Users', () => {
+    beforeAll(async() => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    request(app).post('/users')
+    .send({
+        email: 'user@example.com',
+        name: 'Username'
+    });
+
+});
