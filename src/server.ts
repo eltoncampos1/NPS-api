5d56cc51a8abfba4dc19cde1db10b2453ebd80ff
elton de campos <eltoncampos36@gmail.com>
2021-02-25 22:47:56 -0300

    feat: add router to dealing with routes

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.send('Server OK')
-})
-
-
-app.post('/', (request, response) => {
-    return response.json({ message: 'Dados alterados'} )
-})
+app.use(express.json());
+app.use(router)
 
 app.listen(3333, () => {
     console.log('Server is running');
