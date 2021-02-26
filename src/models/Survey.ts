9e3acd4d2d80858d8f35531d4bc6051b86cab811
elton de campos <eltoncampos36@gmail.com>
2021-02-26 00:07:52 -0300

    refactor: fix entity error adding surveys entity to survey model

--- src/models/Survey.ts
+++ src/models/Survey.ts
@@ -1,6 +1,6 @@
-import { PrimaryColumn, Column, CreateDateColumn } from "typeorm";
+import { PrimaryColumn, Column, CreateDateColumn, Entity } from "typeorm";
 import { v4 as uuid } from 'uuid';
-
+@Entity('surveys')
 class Survey {
 
     @PrimaryColumn()
