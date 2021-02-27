e5daa7951859563a19163c1dcd7ba70d1d3d8a4a
elton de campos <eltoncampos36@gmail.com>
2021-02-27 17:49:44 -0300

    reafctor: move errors to instances of AppError

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -6,6 +6,7 @@ import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUserRepository } from "../repositories/SurveysUserRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
 import SendmailService from "../services/SendmailService";
+import { AppError } from "../errors/AppError";
 
 class SendMailController {
     async execute(request: Request, response: Response) {
@@ -18,17 +19,13 @@ class SendMailController {
         const user = await usersRepository.findOne({ email });
 
         if (!user) {
-            return response.status(400).json({
-                error: 'User does not exists'
-            });
+            throw new AppError("User does not exists")
         }
 
         const survey = await sursveysRepository.findOne({ id: survey_id });
 
         if (!survey) {
-             return response.status(400).json({
-                error: 'Survey does not exists'
-            });
+            throw new AppError("Survey does not exists")
         };
 
         const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsmail.hbs');
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import * as yup from 'yup';
+import { AppError } from '../errors/AppError';
 
 import { UsersRepository } from '../repositories/UsersRepository';
 
@@ -25,7 +26,7 @@ class UserController  {
         });
 
         if (userAlreadyExists) {
-            return response.status(400).json( {error: "User already exists!"} )
+            throw new AppError("User already exists")
         }
 
         const user = usersRepository.create({
