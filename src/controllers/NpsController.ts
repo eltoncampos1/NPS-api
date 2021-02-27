4dbcf562631b9a1ed35274ff3b29767de70f3fd5
elton de campos <eltoncampos36@gmail.com>
2021-02-27 17:00:25 -0300

    chore: I created the method that calculates the average of nps

--- src/controllers/NpsController.ts
+++ src/controllers/NpsController.ts
@@ -1,8 +1,40 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysUserRepository } from "../repositories/SurveysUserRepository";
 
 class NpsController {
     async execute(request: Request, response: Response) {
-        
+        const { survey_id } = request.params;
+
+        const surveysUsersRepository = await getCustomRepository(SurveysUserRepository);
+
+        const surveysUsers = await surveysUsersRepository.find({
+            survey_id,
+        });
+
+        const detractors = surveysUsers.filter(
+            (survey) => survey.value >= 0 && survey.value <= 6
+        ).length;
+
+        const Promoters = surveysUsers.filter(
+            (survey) => survey.value >= 9 && survey.value <= 10
+        ).length;
+
+        const passives = surveysUsers.filter(
+            (survey) => survey.value >= 7 && survey.value <= 8
+        ).length;
+
+        const totalAnsers = surveysUsers.length;
+
+        const calculate = (Promoters - detractors) / totalAnsers;
+
+        return response.json({
+            detractors,
+            Promoters,
+            passives,
+            totalAnsers,
+            nps: calculate
+        });
     }
 }
 export { NpsController };
