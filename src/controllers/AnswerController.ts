11dc2f45078a2f717bd7c3d2b754a9c619ddbc39
elton de campos <eltoncampos36@gmail.com>
2021-02-27 16:37:59 -0300

    feat: create execute method, that checks if exists and save

--- src/controllers/AnswerController.ts
+++ src/controllers/AnswerController.ts
@@ -1,8 +1,29 @@
-import { Request } from "express";
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { SurveysUserRepository } from "../repositories/SurveysUserRepository";
 
 class AnswerController  {
     async execute(request: Request, response: Response) {
-        
+        const { value } = request.params;
+        const { u } = request.query;
+
+        const surveysUsersRepository = getCustomRepository(SurveysUserRepository);
+
+        const surveyUser = await surveysUsersRepository.findOne({
+            id: String(u),
+        });
+
+        if (!surveyUser) {
+            return response.status(400).json({
+                error: "Survey does not exists"
+            });
+        }
+
+        surveyUser.value = Number(value);
+
+        await surveysUsersRepository.save(surveyUser);
+
+        return response.json(surveyUser);
     }
 }
 
